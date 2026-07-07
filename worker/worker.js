2f32ccbe6484aedfb604d20b44e91422fda8302d
Miriad <miriad@miriad.systems>
2026-07-07 17:31:30 +0000

    fix(worker): add PUT to Access-Control-Allow-Methods (curated layout view save)
    Browser preflight was rejecting PUT /curated-layouts/:id/views/:viewKey.
    Redeployed to Cloudflare.

--- worker/worker.js
+++ worker/worker.js
@@ -30,7 +30,7 @@ const ENTERPRISE_QUERY =
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
 
